3516ae29f09cdfb0c993ec76f3d808ecd22ebe3c
erikson <rod>
2020-12-20 17:53:39 -0400

    adjustment in name project Closes #2

--- src/main/java/erod/springframework/SfgPetClinicApplication.java
+++ src/main/java/erod/springframework/SfgPetClinicApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SfgPetClnicApplication {
+public class SfgPetClinicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgPetClnicApplication.class, args);
+		SpringApplication.run(SfgPetClinicApplication.class, args);
 	}
 
 }
