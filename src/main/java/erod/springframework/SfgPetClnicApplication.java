2b8077a1734a00da4032d4e21b9b9ef10f7e6995
erikson <rod>
2020-12-20 08:36:01 -0400

    initial commit

---
+++ src/main/java/erod/springframework/SfgPetClnicApplication.java
@@ -0,0 +1,13 @@
+package erod.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClnicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgPetClnicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/erod/springframework/SfgPetClnicApplicationTests.java
@@ -0,0 +1,13 @@
+package erod.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClnicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
