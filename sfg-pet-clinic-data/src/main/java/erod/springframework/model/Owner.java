db016bb2051f622045dbce1fc589fb2faeb2ab65
erikson879 <rodriguez.erikson@gmail.com>
2021-01-02 22:21:12 -0400

    added contact info to owner entity, closes #26

--- sfg-pet-clinic-data/src/main/java/erod/springframework/model/Owner.java
+++ sfg-pet-clinic-data/src/main/java/erod/springframework/model/Owner.java
@@ -5,5 +5,34 @@ import java.util.Set;
 public class Owner extends Person {
 	
 	private Set<Pet> pets;
+	private String addres;
+	private String city;
+	private String telephone;
+	public Set<Pet> getPets() {
+		return pets;
+	}
+	public void setPets(Set<Pet> pets) {
+		this.pets = pets;
+	}
+	public String getAddres() {
+		return addres;
+	}
+	public void setAddres(String addres) {
+		this.addres = addres;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	
 
 }
