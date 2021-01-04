6a2406c084f05ce7ec2e9e0ce10899fe72536180
erikson879 <rodriguez.erikson@gmail.com>
2021-01-03 23:56:17 -0400

    added complement of issues closes #32

--- sfg-pet-clinic-data/src/main/java/erod/springframework/services/map/VetServiceMap.java
+++ sfg-pet-clinic-data/src/main/java/erod/springframework/services/map/VetServiceMap.java
@@ -4,13 +4,29 @@ import java.util.Set;
 
 import org.springframework.stereotype.Service;
 
+import erod.springframework.model.Specialty;
 import erod.springframework.model.Vet;
+import erod.springframework.services.SpecialtyService;
 import erod.springframework.services.VetService;
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Integer> implements VetService{
 
+	private final SpecialtyService specialtyService; 
+	public VetServiceMap(SpecialtyService specialtyService) {
+		super();
+		this.specialtyService = specialtyService;
+	}
+
 	@Override
 	public Vet save(Vet type) {
+		if(type.getSpecialties().size() > 0 ) {
+			type.getSpecialties().forEach(specialty -> {
+				if(specialty.getId() == null) {
+					Specialty savedSpecialty = specialtyService.save(specialty);
+					specialty.setId(savedSpecialty.getId());
+				}
+			});
+		}
 		return super.save( type);
 	}
 
